d48209e2320f2f4e38e7d2cc886b615727eb454f
junaidq <xcoder27@gmail.com>
2018-04-20 12:59:33 +0500

    Rails 4 Setup and Product Controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot4::Application.config.secret_key_base = 'f085b57831d05f9c9011287bb374db396e204ab7cfe8bd5562e636f5a08d3bbd56214cb1d6011b586f2f1423c9d5d94842f52483d1656a6178d475b5212bdd9f'
